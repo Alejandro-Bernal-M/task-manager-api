c65f5391368583b6305ba7e7593da139db59d453
alejandro <alejandrober96@gmail.com>
2023-06-30 12:20:33 -0500

    Add user scaffold, api/v1/user scaffold and intall devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, class: "Api::V1::User"
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
