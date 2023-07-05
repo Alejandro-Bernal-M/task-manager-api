871b85dabc1dbe512a1aeb63ddea16676441106b
alejandro <alejandrober96@gmail.com>
2023-07-05 07:25:44 -0500

    Routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :tasks do
+      resources :users do
+        resources :tasks
         resources :assignments
+        resources :usergroups
+        resources :groups do
+          resources :subgroups
+        end
       end
-      resources :users
     end
   end
 
