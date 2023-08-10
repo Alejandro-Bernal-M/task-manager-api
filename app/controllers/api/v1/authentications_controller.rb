c071d90e5d38735524fde314acacb12fbcd5446f
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-08-10 11:40:25 -0500

    Return the user_id

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::AuthenticationsController < ApplicationController
     if user && user.authenticate(params[:password])
       expiration_time = 2.hours.from_now.to_i
       token = JWT.encode({user_id: user.id, exp: expiration_time }, Rails.application.secrets.secret_key_base, 'HS256')
-      render json: {status: 'Sucess', token: token}
+      render json: {status: 'Sucess', token: token, user_id: user.id}
     else
       render json: {status: 'Error', message: 'Invalid username or password'}
     end
