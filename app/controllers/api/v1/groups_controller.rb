7d36a7e9799d852211a9dd22b99ce25c7f561d6f
alejandro <alejandrober96@gmail.com>
2023-08-15 15:51:56 -0500

    Add subgroups to the response of the groups

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::GroupsController < ApplicationController
 
   # GET /api/v1/users/:user_id/groups
   def index
-    @groups = Group.where(author_id: params[:user_id])
+    @groups = Group.where(author_id: params[:user_id]).includes(:subgroups)
 
-    render json: {status: 'SUCCESS', data: @groups}
+    render json: {status: 'SUCCESS', data: @groups.map { |group| {group: group, subgroups: group.subgroups} }}
   end
 
   # GET /api/v1/groups/1
@@ -18,7 +18,7 @@ class Api::V1::GroupsController < ApplicationController
     @group = Group.new(group_params)
 
     if @group.save
-      render json: @group, status: :created
+      render json: {data: @group, status: 'SUCCESS'}
     else
       render json: @group.errors, status: :unprocessable_entity
     end
@@ -35,7 +35,11 @@ class Api::V1::GroupsController < ApplicationController
 
   # DELETE /api/v1/groups/1
   def destroy
-    @group.destroy
+    if @group.destroy
+      render json: {status: 'SUCCESS', message: 'Deleted the group', data: @group} 
+    else
+      render json: {status: 'ERROR', message: 'Failed to delete the group', data: @group.errors}
+    end
   end
 
   private
