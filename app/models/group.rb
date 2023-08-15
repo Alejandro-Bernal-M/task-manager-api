6e19ce12599dca242f5be800d45b48a828821032
alejandro <alejandrober96@gmail.com>
2023-08-15 15:51:25 -0500

    Add validations when creating groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   has_many :subgroups, class_name: 'Subgroup', foreign_key: 'group_id', dependent: :destroy
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 3, maximum: 500 }
+  validates :author_id, presence: true
 end
