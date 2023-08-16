068c70829cb18567c80b2526ad6d882025bf25c8
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-08-16 15:57:43 -0500

    Fix user-group model

--- app/models/usergroup.rb
+++ app/models/usergroup.rb
@@ -1,4 +1,4 @@
 class Usergroup < ApplicationRecord
-  belongs_to :user, class_name: 'Api::V1::Users'
-  belongs_to :subgroup, class_name: 'Api::V1::Subgroups'
+  belongs_to :user, class_name: 'User'
+  belongs_to :subgroup, class_name: 'Subgroup'
 end
